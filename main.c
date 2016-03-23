5284e97012640ec70bdede545b7f28625afee7ee
jacobmwhitney1 <jacobmwhitney1@gmail.com>
2016-03-23 18:55:36 -0400

    Added files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+/*************************************************
+Written by..: Jacob W, Tuan D, Collin J, Jason J
+Date written: Mar 23 2016
+Purpose: Exam 3 Grading
+*************************************************/
+#define _CRT_SECURE_NO_WARNINGS
+#define PAUSE system("pause")
+#define CLS system("cls")
+#include <stdio.h>
+#include <stdlib.h>
+
+// ******************Prototype Functions***********************
+
+
+int main() {
+	
+
+} // end main
+
+//*************************************************************
+//*************************************************************
