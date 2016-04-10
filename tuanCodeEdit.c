ff2f9fac54feffa88f7a00f3bf976400e4734fab
m1r4kuru <testlog777@gmail.com>
2016-04-09 21:47:44 -0400

    Update tuanCodeEdit.c

--- tuanCodeEdit.c
+++ tuanCodeEdit.c
@@ -1,5 +1,9 @@
-// Code broken, only let me input 4 scores. No Function yet.
-//***********************************************************
+/*
+* Written by: Tuan Doan, 
+*
+*
+*/
+
 #define _CRT_SECURE_NO_WARNINGS
 #define SIZE 10000
 #define PAUSE system("pause")
@@ -9,40 +13,129 @@
 #include <stdlib.h>
 
 // PROTYPE MY FUNCTIONS HERE
-int getScore(int a[], int c);
-void displayAverage(int c);
-float calcAverage();
+int getScore(int array[], int counter);
+void displayAverage(int counter);
+float calcAverage(int array[], int counter);
+void displayAscending(int array[], int counter, char message[]);
+char doTheSwitch(int array[], int index);
+float calcAscending(int array[], int counter);
 //float sortScores();
 
 main(){
-	int score[SIZE];
+	int array[SIZE];
 	int counter = 0;
-	getScore(score, counter);
-	PAUSE;
+	char message;
+	char userChoice = 'Q';
+	// Display menu
+    do {
+        printf("\n==================================\n");
+        printf("============= MENU ===============\n");
+        printf("==================================\n");
+        printf("A. Enter scores\n");
+        printf("B. Get average scores\n");
+        printf("C. Display scores from high to low\n");
+        printf("Q. Quit\n");
+        scanf(" %c", &userChoice); FLUSH; // clear the buffer in front of %c
+        userChoice = toupper(userChoice); // convert lowercase letter to uppercase
+
+        switch (userChoice){
+        case 'A':
+        getScore(array, counter);
+        break;
+
+        case 'B':
+        CLS; // clear the screen
+       	displayAverage(counter);
+        break;
+
+        case 'C':
+        CLS; // clear the screen
+        displayAscending(array, counter, message);
+        break;
+
+        case 'Q':
+        CLS; // clear the screen
+        printf("Have a good day!\n\n");
+        PAUSE; // pause the program
+        break;
+
+        default:
+        CLS; // clear the screen
+        printf("Invalid choice, please only enter from A - Q\n\n");
+        } // end switch
+
+    } while (userChoice != 'Q'); // do loop for all the cases A, B, C, D, E, except Q
+
+return 0;
+PAUSE; // pause the program
 } // end main
 
-float calcAverage() {
+int getScore(int array[], int counter) {
+	int result = 0;
+	printf("Enter a score: \n"); // get input
+    scanf("%i", &array[counter]);
+    counter++;
+	return result;
+} // end getScore
+
+float calcAverage(int array[], int counter) {
+    int i = 0;
+    float sum = 0;
+	float avg = 0;
 	float result = 0;
-	for (i = 0; i < (sizeof score[i]); i++)
+	for (i = 0; i < counter; i++)
 	{
-		sum += score[i]; //get sum
-		avg = sum / (sizeof score[i]); // get average
+		sum += array[i]; //get sum
+		avg = sum / array[i]; // get average
 	}
 	return result;
 } // end calcAverage
 
-int getScore(int a[], int c) {
-	int result = 0;
-	printf("Enter a score: \n"); // get input
-		scanf("%i", &a[c]);
-	return result;
-} // end getScore
+void displayAverage(int counter) {
+    float avg;
 
-void displayAverage(int c) {
-
-	if (c == 0) // check if there is no input score
+	if (counter == 0) // check if there is no input score
 		printf("Please enter a score first.\n\n");
 
 	else
 		printf("The average score is %.2f\n\n", avg); // display average score
 } // end displayAverage
+
+float calcAscending(int array[], int counter){
+
+  int i, bottom;
+  char switchMade;
+
+  bottom = counter - 1;
+
+  do {
+     switchMade = 'N';
+     for(i=0; i < bottom; i++) {
+        if(array[i] > array[i+1])
+           switchMade = doTheSwitch(array, i);
+     } // end for
+     bottom--;
+   }while(switchMade == 'Y');
+
+  displayAscending(array, counter,  "THE SORTED LIST");
+
+  PAUSE;
+
+} // end calcAscending
+
+void displayAscending(int a[], int counter, char message[]) {
+     int i;
+     printf("%s: \n", message);
+     for (i=0; i < counter; i++)
+     printf("Element %2i = %2i.\n", i+1, a[i]);
+     printf("\n");
+} // end of displayAscending
+
+char doTheSwitch(int array[], int i) {
+    char result = 'Y';
+    int temp;
+    temp = array[i];
+    array[i] = array[i+1];
+    array[i+1]= temp;
+    return result;
+} // end function doTheSwitch
